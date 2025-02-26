453e72cae23453c4f83a3b4f78c7eeed96625e37
chaves <chavesprogrammer@gmail.com>
2025-02-25 22:24:10 -0300

    estrutura do projeto

---
+++ src/main/java/com/agendarai/Agenda/Facil/AgendaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.agendarai.Agenda.Facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendarai/Agenda/Facil/AgendaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendarai.Agenda.Facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
