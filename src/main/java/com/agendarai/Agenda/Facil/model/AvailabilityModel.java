dbddad4affdccb6b91415a3e7ad4d8cd9075b088
chaves <chavesprogrammer@gmail.com>
2025-03-04 21:08:04 -0300

    availability model created

--- src/main/java/com/agendarai/Agenda/Facil/model/AvailabilityModel.java
+++ src/main/java/com/agendarai/Agenda/Facil/model/AvailabilityModel.java
@@ -1,4 +1,32 @@
 package com.agendarai.Agenda.Facil.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="tb_availability")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AvailabilityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    @Column(nullable = false)
+    private Boolean isAvailable = true;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id", nullable = false)
+    private UserModel professor;
+
+
 }
