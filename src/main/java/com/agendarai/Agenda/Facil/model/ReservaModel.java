3dfc75b96d6999ab06128c88c7b35a68f4ae3b0e
chaves <chavesprogrammer@gmail.com>
2025-03-04 21:09:07 -0300

    reserva model created

--- src/main/java/com/agendarai/Agenda/Facil/model/ReservaModel.java
+++ src/main/java/com/agendarai/Agenda/Facil/model/ReservaModel.java
@@ -1,4 +1,30 @@
 package com.agendarai.Agenda.Facil.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_reserva")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ReservaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id", nullable = false)
+    private UserModel professor;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private UserModel aluno;
+
+    @OneToOne
+    @JoinColumn(name = "availability_id", nullable = false, unique = true)
+    private AvailabilityModel availability;
 }
