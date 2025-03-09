3d9475b32bffd7927eb52a0aae130ca8b909550d
chaves <chavesprogrammer@gmail.com>
2025-03-09 12:34:32 -0300

    add availabilitYRepository

--- src/main/java/com/agendarai/Agenda/Facil/repository/AvailabilityRepository.java
+++ src/main/java/com/agendarai/Agenda/Facil/repository/AvailabilityRepository.java
@@ -1,4 +1,16 @@
 package com.agendarai.Agenda.Facil.repository;
 
-public interface AvaibilityRepository {
+import com.agendarai.Agenda.Facil.model.AvailabilityModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AvailabilityRepository extends JpaRepository<AvailabilityModel, UUID> {
+    List<AvailabilityModel> findByPersonalIdAndIsAvailable(UUID personalId, boolean isAvailable);
+    List<AvailabilityModel> findByDateTimeBetween(LocalDateTime start, LocalDateTime end);
 }
+
