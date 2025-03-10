8c39e24d347d77d5f323994e031f52332b762fd3
chaves <chavesprogrammer@gmail.com>
2025-03-10 01:28:26 -0300

    add DTOs

--- src/main/java/com/agendarai/Agenda/Facil/dto/AvailabilityDTO.java
+++ src/main/java/com/agendarai/Agenda/Facil/dto/AvailabilityDTO.java
@@ -1,4 +1,19 @@
 package com.agendarai.Agenda.Facil.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AvailabilityDTO {
+    private UUID id;
+    private LocalDateTime dateTime;
+    private boolean isAvailable;
+    private UUID personalId;
+
 }
--- src/main/java/com/agendarai/Agenda/Facil/dto/ReservaDTO.java
+++ src/main/java/com/agendarai/Agenda/Facil/dto/ReservaDTO.java
@@ -1,4 +1,19 @@
 package com.agendarai.Agenda.Facil.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ReservaDTO {
+    private UUID id;
+    private UUID alunoId;
+    private UUID availabilityId;
+    private String alunoName;
+    private LocalDateTime dateTime;
 }
